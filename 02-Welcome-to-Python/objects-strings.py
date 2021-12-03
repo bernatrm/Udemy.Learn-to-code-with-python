a38803984930df2a99966d17d3dd9928e19b7b74
Bernat Real <bernat.real@hotelsviva.com>
2021-12-03 09:51:59 +0100

    Finalizada Sesion 2

---
+++ 02-Welcome-to-Python/objects-strings.py
@@ -0,0 +1,19 @@
+"sushi"
+'sushi'
+'warriorsfan123!'
+'$5.00'
+
+"You're right"
+'Shakespeare once wrote "To be or not to be" in one of his plays'
+
+" "
+"" #empty string
+'' #empty string
+
+"5" #string
+5 #number
+
+"""Hello
+me name is 
+Bernat
+""" #break text over various lines
