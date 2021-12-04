455cef73e83d81cfb1e571fe33832d278fd8116a
Bernat Real <bernat.real@hotelsviva.com>
2021-12-04 09:01:34 +0100

    Finalizada Sección 3

---
+++ 03-Numbers-Booleans-and-Equality/division-floor-division-and-the-modulo-operator.py
@@ -0,0 +1,16 @@
+print(15 / 3)
+print(14 / 3)
+
+# Floor Division: In mathematics and computer science, 
+# the floor function is the function that takes as input 
+# a real number x, and gives as output the greatest integer less than or equal to x
+print(14 // 3) #floor division: integer part of the result
+print(-14 // 3) #floor division: rounded integer part of the result
+
+print(14 % 3) #MOD: Divide and get remainder
+print(15 % 3)
+print(16 % 3)
+
+print(1 % 3)
+print(2 % 3)
+print(3 % 3)
