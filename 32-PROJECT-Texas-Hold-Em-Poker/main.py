49d5bf6c1f9768aa7a40987b8f61c0506e045f94
Bernat Real <bernat.real@hotelsviva.com>
2022-09-15 07:47:31 +0200

    Iteration 1

---
+++ 32-PROJECT-Texas-Hold-Em-Poker/main.py
@@ -0,0 +1,9 @@
+from distutils.debug import DEBUG
+from poker.card import Card
+from poker.deck import Deck
+
+deck = Deck()
+cards = Card.create_standard_52_cards()
+deck.add_cards(cards)
+
+# from main import deck, cards
