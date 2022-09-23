2d83f506e57e78a0cd363c705d181e2e28b57f0d
Bernat Real <bernat.real@hotelsviva.com>
2022-09-23 18:50:31 +0200

    Iteration5: Fixing Bugs

--- 32-PROJECT-Texas-Hold-Em-Poker/main.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/main.py
@@ -1,9 +1,17 @@
-from distutils.debug import DEBUG
 from poker.card import Card
 from poker.deck import Deck
+from poker.hand import Hand
+from poker.player import Player
 
 deck = Deck()
 cards = Card.create_standard_52_cards()
 deck.add_cards(cards)
 
-# from main import deck, cards
+hand1 = Hand(cards = [])
+hand2 = Hand(cards = [])
+
+player1 = Player(name= "Bernat", hand= hand1) 
+player2 = Player(name= "Bobby", hand= hand2) 
+
+
+# from main import deck, cards, hand1, hand2, player1, player2
---
+++ 32-PROJECT-Texas-Hold-Em-Poker/tests/test_player.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import MagicMock
+
+from poker.hand import Hand
+from poker.player import Player
+
+class PlayerTest(unittest.TestCase):
+    def test_store_name_and_hand(self):
+        hand = Hand(cards = [])
+        player = Player(name = "Bernat", hand = hand)
+        self.assertEqual(player.name, "Bernat")
+        self.assertEqual(player.hand, hand)
+
+    def test_figures_out_own_best_hand(self):
+        mock_hand = MagicMock()
+        mock_hand.best_rank.return_value = "Straight Flush"
+
+        player = Player(name = "Bernat", hand = mock_hand)
+
+        self.assertEqual(
+            player.best_hand(),
+            "Straight Flush"
+        )
+        
+        mock_hand.best_rank.assert_called()
