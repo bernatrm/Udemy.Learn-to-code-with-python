b4a6cfb089acfd835550c21d46d0fcd0e46bfb3b
Bernat Real <bernat.real@hotelsviva.com>
2022-09-25 19:04:32 +0200

    Iteration8: Adding Community_Cards

--- 32-PROJECT-Texas-Hold-Em-Poker/poker/game_round.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/poker/game_round.py
@@ -4,14 +4,11 @@ class GameRound():
         self.players = players
 
     def play(self):
-        # Shuffle the deck
         self._shuffle_deck()
-        
-        # Assign two cards to every player
         self._deal_initial_two_cards_to_each_player()
-
-        # Ask for wagers
         self._make_bets()
+
+        self._deal_flop_cards()
 
     def _shuffle_deck(self):
         self.deck.shuffle()
@@ -25,3 +22,8 @@ class GameRound():
         for player in self.players:
             if player.wants_to_fold():
                 self.players.remove(player)
+
+    def _deal_flop_cards(self):
+        community_cards = self.deck.remove_cards(3)
+        for player in self.players:
+            player.add_cards(community_cards)
