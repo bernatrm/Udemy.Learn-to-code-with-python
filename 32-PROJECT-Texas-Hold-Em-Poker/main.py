396bec15bf9af7e1da81985d397100ee06388b92
Bernat Real <bernat.real@hotelsviva.com>
2022-10-09 20:48:30 +0200

    Iteration12: Winner Algorithm

--- 32-PROJECT-Texas-Hold-Em-Poker/main.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/main.py
@@ -10,12 +10,23 @@ deck.add_cards(cards)
 
 hand1 = Hand()
 hand2 = Hand()
+hand3 = Hand()
 
 player1 = Player(name= "Bernat", hand= hand1) 
-player2 = Player(name= "Bobby", hand= hand2) 
-players = [player1, player2]
+player2 = Player(name= "Bobby", hand= hand2)
+player3 = Player(name= "Llorenç", hand= hand3)
+players = [player1, player2, player3]
 
 game_round = GameRound(deck=deck, players=players)
 game_round.play()
 
-# from main import deck, cards, game_round, hand1, hand2, player1, player2
+for player in players:
+    print(f"{player.name} revices a {player.hand}.")
+    index, hand_name, hand_cards = player.best_hand()
+    hand_card_strings = [str(card) for card in hand_cards]
+    hand_card_strings = " and ".join(hand_card_strings)
+    print(F"{player.name} has a {hand_name} with a {hand_card_strings}.")
+
+winning_player = max(players)
+
+print(f"The winner es {winning_player.name}.")
--- 32-PROJECT-Texas-Hold-Em-Poker/poker/player.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/poker/player.py
@@ -3,6 +3,12 @@ class Player():
         self.name = name
         self.hand = hand
 
+    def __gt__(self, other: object) -> bool:
+        current_player_best_validator_index = self.best_hand()[0]
+        other_player_best_validator_index = other.best_hand()[0]
+
+        return current_player_best_validator_index < other_player_best_validator_index
+
     def best_hand(self) -> str:
         return self.hand.best_rank()
 
@@ -11,6 +17,7 @@ class Player():
 
     def wants_to_fold(self) -> bool:
         return False
+
 
 
     
